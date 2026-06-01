31ff0597615d5e84cfdcdcdd6ab000aae1758832
isaacschuey <isaacschuey@gmail.com>
2026-06-01 09:33:47 -0500

    Sidebar items

--- src/components/ui/sidebar-link/SidebarLink.tsx
+++ src/components/ui/sidebar-link/SidebarLink.tsx
@@ -1,11 +1,17 @@
 import { NavLink } from "react-router-dom";
 
+import "./SidebarLink.css";
+
 interface SidebarLinkProps {
   sportAbbrev: string;
 }
 
 const SidebarLink = (props: SidebarLinkProps) => {
-  return <NavLink to={`/${props.sportAbbrev.toLowerCase()}`}>{props.sportAbbrev}</NavLink>;
+  return (
+    <NavLink to={`/${props.sportAbbrev.toLowerCase()}`}>
+      {props.sportAbbrev}
+    </NavLink>
+  );
 };
 
 export default SidebarLink;
